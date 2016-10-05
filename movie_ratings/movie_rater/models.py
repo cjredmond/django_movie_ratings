565539db2cdb4f13d16bd2032e5c2124dd1c14ed
cjredmond <connor.redmond@gmail.com>
2016-10-05 13:44:33 -0400

    migrated Rater and Movie class

--- movie_ratings/movie_rater/models.py
+++ movie_ratings/movie_rater/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length = 50)
+    release_date = models.CharField(max_length = 50)
+    video_release = models.CharField(max_length = 40)
+    url = models.CharField(max_length = 150)
+    unknown = models.CharField(max_length = 50)
+    action_genre = models.IntegerField()
+    adventure = models.IntegerField()
+    animation = models.IntegerField()
+    children = models.IntegerField()
+    comedy = models.IntegerField()
+    crime = models.IntegerField()
+    documentary = models.IntegerField()
+    drama = models.IntegerField()
+    fantasy = models.IntegerField()
+    film_noir = models.IntegerField()
+    horror = models.IntegerField()
+    musical = models.IntegerField()
+    mystery = models.IntegerField()
+    romance = models.IntegerField()
+    sci_fi = models.IntegerField()
+    thriller = models.IntegerField()
+    war = models.IntegerField()
+    western = models.IntegerField()
+
+class Rater(models.Model):
+    rater_id = models.IntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length = 1)
+    occupation = models.CharField(max_length = 50)
+    zip_code = models.CharField(max_length = 40)
